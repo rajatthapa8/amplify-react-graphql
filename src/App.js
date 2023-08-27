9be2f38a90f4b9f25f284039225680d686d909c0
Rajat Thapa <rajatthapa826@gmail.com>
2023-08-27 23:52:15 +1000

    second commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h3>Hello AWS Amplify</h3>
+        <h3>Hello AWS Amplify!!!</h3>
       </header>
     </div>
   );
